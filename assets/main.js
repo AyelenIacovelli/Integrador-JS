a8a0ee74fe0f3e9360d83e4a4ff78d044b450f08
Ayelén Iacovelli <iacovelli.ayelen@gmail.com>
2023-01-23 16:45:53 -0300

    continuo trabajo

--- assets/main.js
+++ assets/main.js
@@ -10,106 +10,106 @@ const cartMenu = document.querySelector(".cart")
 const barsMenu = document.querySelector(".navbar-list");
 // OVERLAY
 const overlay = document.querySelector(".overlay");
+// HEADER
+const header = document.querySelector("header");
 
 // LOCALSTORAGE
 let cart = JSON.parse(localStorage.getItem("cart")) || [];
 
 // FUNCION DE GUARGADO EN LOCALSTORAGE
 const saveLocalStorage = (cartList) => {
-    localStorage.setItem("cart", JSON.stringify(cartList));
+    localStorage.setItem(`cart`, JSON.stringify(cartList));
 };
 
 // FUNCION CLICK MENU HAMBURGUESA
 const toggleMenu = () => {
-    barsMenu.classList.toggle("open-menu");
-    if (cartMenu.classList.contains("open-cart")) {
-        cartMenu.classList.remove("open-cart");
+    barsMenu.classList.toggle(`open-menu`);
+    if (cartMenu.classList.contains(`open-cart`)) {
+        cartMenu.classList.remove(`open-cart`);
         return;
     }
-    overlay.classList.toggle("show-overlay");
+    overlay.classList.toggle(`show-overlay`);
+    header.classList.toggle(`header-bg`);
 };
 
 // FUNCION CLICK CARRITO
 const toggleCart = () => {
-    cartMenu.classList.toggle("open-cart");
-    if (barsMenu.classList.contains("open-menu")) {
-        barsMenu.classList.remove("open-menu");
+    cartMenu.classList.toggle(`open-cart`);
+    if (barsMenu.classList.contains(`open-menu`)) {
+        barsMenu.classList.remove(`open-menu`);
         return;
     }
-    overlay.classList.toggle("show-overlay");
+    overlay.classList.toggle(`show-overlay`);
+    header.classList.toggle(`header-bg`);
 };
 
 // FUNCION CERRAR CARRITO CUANDO SE CLICKEA NAVBAR
 const closeOnClick = (e) => {
-    if (!e.target.classList.contains("navbar-link")) {
+    if (!e.target.classList.contains(`navbar-link`)) {
         return;
     }
-    barsMenu.classList.remove("open-menu");
-    overlay.classList.remove("show-overlay");
+    barsMenu.classList.remove(`open-menu`);
+    overlay.classList.remove(`show-overlay`);
+    header.classList.remove(`header-bg`);
 }
 
 // FUNCION PARA CERRAR TODO AL SCROLLEAR
 const closeOnScroll = () => {
-    if (!barsMenu.classList.contains("open-menu") && !cartMenu.classList.contains("open-cart")) {
+    if (!barsMenu.classList.contains(`open-menu`) && !cartMenu.classList.contains(`open-cart`)) {
         return;
     }
-    barsMenu.classList.remove("open-menu");
-    cartMenu.classList.remove("open-cart");
-    overlay.classList.remove("show-overlay");
+    barsMenu.classList.remove(`open-menu`);
+    cartMenu.classList.remove(`open-cart`);
+    overlay.classList.remove(`show-overlay`);
+    header.classList.remove(`header-bg`);
 };
 
 // FUNCION PARA CERRAR TODO AL CLICKEAR EN OVERLAY
 const closeOnOverlayClick = (e) => {
-    barsMenu.classList.remove("open-menu");
-    cartMenu.classList.remove("open-cart");
-    overlay.classList.remove("show-overlay");
+    barsMenu.classList.remove(`open-menu`);
+    cartMenu.classList.remove(`open-cart`);
+    overlay.classList.remove(`show-overlay`);
+    header.classList.remove(`header-bg`);
 };
 
 
 // CAMBIO COLOR HEADER
-const header = document.querySelector("header");
-
-const scrollHeader = () => {
-    header.classList.toggle("down", window.scrollY > 0);
-}
-
+window.addEventListener("scroll", function () {
+    const header = document.querySelector("header");
+    header.classList.toggle(`header-bg`, window.scrollY > 350);
+});
 
 // SLIDER EN NOTICIAS
-const slider = document.querySelector("#slider");
-let sliderSection = document.querySelectorAll(".card-info");
-let sliderSectionLast = sliderSection[sliderSection.length - 1];
-const chevronPost = document.querySelector("#chevron-post");
-const chevronPre = document.querySelector("#chevron-pre");
+// const slider = document.querySelector("#slider");
+// let sliderSection = document.querySelectorAll(".card-info");
+// let sliderSectionLast = sliderSection[sliderSection.length - 1];
+// const chevronPost = document.querySelector("#chevron-post");
+// const chevronPre = document.querySelector("#chevron-pre");
 
 // PONGO ULTIMA IMG COMO PRIMERA
-slider.insertAdjacentElement("afterbegin", sliderSectionLast);
+// slider.insertAdjacentElement("afterbegin", sliderSectionLast);
 
 // FUNCION PARA MOVER SIGUIENTE
-const next = () => {
-    let sliderSectionFirst = document.querySelectorAll(".card-info")[0];
-    slider.style.marginLeft = "-200%";
-    slider.insertAdjacentElement("beforeend", sliderSectionFirst);
-    slider.style.marginLeft = "-100%";
-
-}
+// const next = () => {
+//     let sliderSectionFirst = document.querySelectorAll(".card-info")[0];
+//     slider.style.marginLeft = "-200%";
+//     slider.insertAdjacentElement("beforeend", sliderSectionFirst);
+//     slider.style.marginLeft = "-100%";
+// }
 
 // FUNCION PARA MOVER ANTERIOR
-const prev = () => {
-    let sliderSection = document.querySelectorAll(".card-info");
-    let sliderSectionLast = sliderSection[sliderSection.length - 1];
-    slider.style.marginLeft = "0";
-    slider.insertAdjacentElement("afterbegin", sliderSectionLast);
-    slider.style.marginLeft = "-100%";
-
-}
+// const prev = () => {
+//     let sliderSection = document.querySelectorAll(".card-info");
+//     let sliderSectionLast = sliderSection[sliderSection.length - 1];
+//     slider.style.marginLeft = "0";
+//     slider.insertAdjacentElement("afterbegin", sliderSectionLast);
+//     slider.style.marginLeft = "-100%";
+// }
 
 // FUNCION PARA QUE SE MUEVA AUTOMATICAMENTE
-setInterval = (function () {
-    Next();
-}, 5000);
-
-
-
+// setInterval = (function () {
+//     Next();
+// }, 5000);
 
 // MOSTRAR PRODUCTOS EN PAGINA
 
@@ -136,7 +136,7 @@ const renderProduct = (product) => {
                     <button>+ y -</button>
                 </div>
             </div>
-        </div>
+    </div>
     `;
 }
 
@@ -165,10 +165,10 @@ const renderProducts = (index = 0, category = undefined) => {
 // FUNCION QUE MUESTRA U OCULTA BTN VER MAS
 const changeShowMoreBtnState = (category) => {
     if (!category) {
-        btnLoad.classList.remove("hidden");
+        btnLoad.classList.remove(`hidden`);
         return;
     }
-    btnLoad.classList.add("hidden");
+    btnLoad.classList.add(`hidden`);
 };
 
 // FUNCION PARA CAMBIAR COLOR DE BTN DE CATEGORIA SELECCIONADA
@@ -176,10 +176,10 @@ const changeBtnActive = (selectedCategory) => {
     const categories = [...categoriesList];
     categories.forEach((categoryBtn) => {
         if (categoryBtn.dataset.category !== selectedCategory) {
-            categoryBtn.classList.remove("active");
+            categoryBtn.classList.remove(`active`);
             return;
         };
-        categoryBtn.classList.add("active");
+        categoryBtn.classList.add(`active`);
     });
 };
 
@@ -192,7 +192,7 @@ const changeFilterState = (e) => {
 
 // FUNCION PARA APLICAR FILTRO
 const applyFilter = (e) => {
-    if (!e.target.classList.contains("category")) {
+    if (!e.target.classList.contains(`category`)) {
         return;
     } else {
         changeFilterState(e);
@@ -218,7 +218,7 @@ const showMoreProducts = () => {
     renderProducts(productsController.nextProductsIndex);
     productsController.nextProductsIndex++;
     if (isLastIndexOf()) {
-        btnLoad.classList.add("hidden");
+        btnLoad.classList.add(`hidden`);
     }
 };
 
@@ -226,16 +226,22 @@ const showMoreProducts = () => {
 
 // FUNCION PUERTA DE ENTRADA
 const init = () => {
-    menuBtn.addEventListener("click", toggleMenu);
-    cartBtn.addEventListener("click", toggleCart);
-    barsMenu.addEventListener("click", closeOnClick);
-    window.addEventListener("scroll", closeOnScroll);
-    overlay.addEventListener("click", closeOnOverlayClick);
+    menuBtn.addEventListener(`click`, toggleMenu);
+    cartBtn.addEventListener(`click`, toggleCart);
+    barsMenu.addEventListener(`click`, closeOnClick);
+    window.addEventListener(`scroll`, closeOnScroll);
+    overlay.addEventListener(`click`, closeOnOverlayClick);
     renderProducts();
-    window.addEventListener("scroll", scrollHeader);
+    categories.addEventListener(`click`, applyFilter);
+    btnLoad.addEventListener(`click`, showMoreProducts);
+
+    chevronPost.addEventListener(`click`, next);
+    chevronPre.addEventListener(`click`, prev);
+
+    renderProducts();
     categories.addEventListener("click", applyFilter);
     btnLoad.addEventListener("click", showMoreProducts);
 
-    chevronPost.addEventListener("click", next);
-    chevronPre.addEventListener("click", prev);
 };
+
+init();
